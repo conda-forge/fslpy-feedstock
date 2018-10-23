5db9712c7b94024982ad3d8b80a3ef28651b4f7f
Paul McCarthy <pauldmccarthy@gmail.com>
2018-10-23 15:16:38 +0100

    Add dcm2niix as optional dependency, add $FSLDIR post-link/pre-unlink scripts.

---
+++ recipe/post-link.sh
@@ -0,0 +1,3 @@
+if [ -e ${FSLDIR}/etc/fslconf/requestFSLpythonLink.sh ]; then
+    $FSLDIR/etc/fslconf/requestFSLpythonLink.sh imcp immv imglob atlasquery atlasq fsl_ents
+fi
---
+++ recipe/pre-unlink.sh
@@ -0,0 +1,3 @@
+if [ -e ${FSLDIR}/etc/fslconf/requestFSLpythonUnlink.sh ]; then
+    $FSLDIR/etc/fslconf/requestFSLpythonUnlink.sh imcp immv imglob atlasquery atlasq fsl_ents
+fi
