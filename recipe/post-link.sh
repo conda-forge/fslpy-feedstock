0f796a1faf42ce2094cff2521035bbfcd4e0a34b
Paul McCarthy <pauldmccarthy@gmail.com>
2019-05-13 12:32:34 +0100

    Pre-post unlink/link for resample_image

--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -1,3 +1,11 @@
 if [ -e ${FSLDIR}/etc/fslconf/requestFSLpythonLink.sh ]; then
-    $FSLDIR/etc/fslconf/requestFSLpythonLink.sh imcp immv imglob atlasquery atlasq fsl_ents
+  $FSLDIR/etc/fslconf/requestFSLpythonLink.sh \
+    imcp          \
+    immv          \
+    imglob        \
+    atlasq        \
+    atlasquery    \
+    extract_noise \
+    fsl_ents      \
+    resample_image
 fi
--- recipe/pre-unlink.sh
+++ recipe/pre-unlink.sh
@@ -1,3 +1,11 @@
 if [ -e ${FSLDIR}/etc/fslconf/requestFSLpythonUnlink.sh ]; then
-    $FSLDIR/etc/fslconf/requestFSLpythonUnlink.sh imcp immv imglob atlasquery atlasq fsl_ents
+  $FSLDIR/etc/fslconf/requestFSLpythonUnlink.sh \
+    imcp          \
+    immv          \
+    imglob        \
+    atlasq        \
+    atlasquery    \
+    extract_noise \
+    fsl_ents      \
+    resample_image
 fi
