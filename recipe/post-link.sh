b66eeab88df9de2311967de9215ee6700ad76f1f
Paul McCarthy <pauldmccarthy@gmail.com>
2026-01-14 10:02:35 +1030

    Bump to 3.27.0, update entry points

--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -1,26 +1,18 @@
-# FSL 6.0.5 and older
-if [ -e ${FSLDIR}/etc/fslconf/requestFSLpythonLink.sh ]; then
-  $FSLDIR/etc/fslconf/requestFSLpythonLink.sh \
-    imcp          \
-    immv          \
-    imglob        \
-    atlasq        \
-    atlasquery    \
-    fsl_ents
-# FSL 6.0.6 and newer
-elif  [ -e ${FSLDIR}/share/fsl/sbin/createFSLWrapper ]; then
+if  [ -e ${FSLDIR}/share/fsl/sbin/createFSLWrapper ]; then
   $FSLDIR/share/fsl/sbin/createFSLWrapper \
-    imcp          \
-    imln          \
-    immv          \
-    imrm          \
-    imglob        \
-    imtest        \
-    remove_ext    \
-    fsl_abspath   \
     Text2Vest     \
     Vest2Text     \
     atlasq        \
     atlasquery    \
-    fsl_ents
+    fsl_abspath   \
+    fsl_ents      \
+    fslchfiletype \
+    imcp          \
+    imglob        \
+    imln          \
+    immv          \
+    imrm          \
+    imtest        \
+    remove_ext    \
+    tmpnam
 fi
